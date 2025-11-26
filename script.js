bff48b900c6e017ac1164c8288c2948259551997
Alankar Singh <alankar010499@gmail.com>
2025-11-26 13:52:06 +0530

    made changes to script

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function makeid(l) {
   // write your code here
+  let string = "";
+  while (string.length < l) {
+    string += Math.random().toString(36).slice(2);
+  }
+  return string.slice(0, length);
 }
 
 // Do not change the code below.
